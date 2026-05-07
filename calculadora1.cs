a12fdc6499c1a6a9b3329299c23c9b4903a9713b
vt <victor.m.costa12@aluno.senai.br>
2026-05-07 15:12:30 -0300

    Add files via upload

---
+++ calculadora1.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("=== Calculadora Escolar ===");
+
+Console.Write("Digite a primeira nota: ");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a segunda nota: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+
+double soma = nota1 + nota2;
+
+Console.WriteLine($"Soma das notas: {soma}");
